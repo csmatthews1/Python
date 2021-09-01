8b6884d741be78cf943c64a91740c8bdc55594fb
csmatthews1 <86792473+csmatthews1@users.noreply.github.com>
2021-09-01 14:50:31 -0500

    Math Dojo exercise

---
+++ fundamentals/extras/mathdojo.py
@@ -0,0 +1,22 @@
+class MathDojo:
+    def __init__(self):
+        self.result = 0
+    def add(self, num, *nums):
+        self.result += num
+        for each in nums:
+            self.result += each
+        return self;
+    def subtract(self, num, *nums):
+        self.result -= num
+        for each in nums:
+            self.result -= each
+        return self
+
+# create an instance:
+md = MathDojo()
+# to test:
+x = md.add(2).add(2,5,1).subtract(3,2).result
+print(x)	# should print 5
+# run each of the methods a few more times and check the result!
+y = md.add(25).add(3,4,5,8).subtract(6,2,2).result 
+print(y) #expect 40
