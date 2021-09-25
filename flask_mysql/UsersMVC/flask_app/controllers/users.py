b7158aa2d847a771f6bcb4981495859e08d45b2c
csmatthews1 <86792473+csmatthews1@users.noreply.github.com>
2021-09-25 09:42:03 -0500

    modularization exercise

---
+++ flask_mysql/UsersMVC/flask_app/controllers/users.py
@@ -0,0 +1,50 @@
+from flask_app import app
+from flask import Flask,render_template, redirect, request
+from flask_app.models.user import User
+
+@app.route('/')
+def index():
+    return redirect('/users')
+
+@app.route('/users')
+def users():
+    # call the get all classmethod to get all users
+    users = User.get_all()
+    return render_template("index.html", all_users = users)
+
+@app.route('/user/new')
+def newuser():
+    return render_template("newuser.html")
+
+@app.route('/user/create',methods=['POST'])
+def create_user():
+    User.save(request.form)
+    return redirect ('/users')
+
+@app.route('/user/<int:id>')
+def show_user(id):
+    data = {
+        "id":id
+    }
+    user = User.get(data)
+    return render_template("showuser.html", current_user = user)
+
+@app.route('/user/<int:id>/edit/')
+def edit(id):
+    data ={ 
+        "id":id
+    }
+    return render_template("edituser.html",user=User.get(data))
+
+@app.route('/user/update',methods=['POST'])
+def update():
+    User.update(request.form)
+    return redirect('/users')
+
+@app.route('/user/<int:id>/delete')
+def delete_user(id):
+    data ={
+        'id': id
+    }
+    User.delete(data)
+    return redirect('/users')
